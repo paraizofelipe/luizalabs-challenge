7b6c2dbd6f867bdc4bb06dd788b5b28ad1a9652c
Paraizo <felipeparaizo@gmail.com>
2021-06-21 10:07:26 -0300

    update modules and create a entry poit of application

--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.16
 require (
 	github.com/google/uuid v1.2.0
 	github.com/jmoiron/sqlx v1.3.4
+	github.com/lib/pq v1.2.0
 )
--- main.go
+++ main.go
@@ -1,4 +1,38 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
+	buyer "github.com/paraizofelipe/luizalabs-challenge/buyer/handler"
+	"github.com/paraizofelipe/luizalabs-challenge/config"
+	product "github.com/paraizofelipe/luizalabs-challenge/product/handler"
+)
+
 func main() {
+	var err error
+	logger := log.New(os.Stdout, "", log.LstdFlags|log.Lshortfile)
+
+	storage, err := sqlx.Open("postgres", config.Storage)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	buyerHandler := buyer.NewHandler(storage, logger)
+	productHandler := product.NewHandler(storage, logger)
+
+	http.HandleFunc("/api/buyer/", buyerHandler.Router)
+	http.HandleFunc("/api/produdct/", productHandler.Router)
+
+	url := fmt.Sprintf("%s:%s", config.Host, os.Getenv("PORT"))
+
+	log.Printf("Server listening in %s", url)
+
+	if err = http.ListenAndServe(url, nil); err != nil {
+		logger.Fatal(err)
+	}
 }
