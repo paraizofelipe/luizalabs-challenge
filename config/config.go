2d45d295bf556735ad64ddf2a049884afd116ac8
Paraizo <felipeparaizo@gmail.com>
2021-06-21 09:59:35 -0300

    adds package to set env vars

---
+++ config/config.go
@@ -0,0 +1,29 @@
+package config
+
+import (
+	"os"
+	"strconv"
+)
+
+var (
+	Host    = os.Getenv("HOST")
+	Port    = EnvToInt(os.Getenv("PORT"))
+	Storage = os.Getenv("STORAGE")
+	Debug   = EnvToBool(os.Getenv("DEBUG"))
+)
+
+func EnvToBool(env string) bool {
+	b, err := strconv.ParseBool(env)
+	if err != nil {
+		return false
+	}
+	return b
+}
+
+func EnvToInt(env string) int {
+	i, err := strconv.Atoi(env)
+	if err != nil {
+		return 0
+	}
+	return i
+}
